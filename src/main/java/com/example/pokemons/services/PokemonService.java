c5bf57ea93d8d05423a7f3ebbb0f5c77c65b9a41
Patryk Prentki <patryk.prentki90@gmail.com>
2025-11-23 22:29:35 +0100

    Created services

---
+++ src/main/java/com/example/pokemons/services/PokemonService.java
@@ -0,0 +1,9 @@
+package com.example.pokemons.services;
+
+import com.example.pokemons.repositories.PokemonRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PokemonService {
+    private PokemonRepository pokemonRepository;
+}
