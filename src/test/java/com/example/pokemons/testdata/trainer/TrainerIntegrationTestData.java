dabf64a77f75a1b5064fbbc81a76bfa36f43f2df
Patryk Prentki <patryk.prentki90@gmail.com>
2025-12-01 00:10:29 +0100

    Prepared test data for Integration tests for Trainer entity

---
+++ src/test/java/com/example/pokemons/testdata/trainer/TrainerIntegrationTestData.java
@@ -0,0 +1,36 @@
+package com.example.pokemons.testdata.trainer;
+
+import com.example.pokemons.entities.Trainer;
+import org.junit.jupiter.params.provider.Arguments;
+
+import java.util.stream.Stream;
+
+public class TrainerIntegrationTestData {
+
+    public static Stream<Arguments> validTrainer() {
+        return Stream.of(
+                Arguments.of(Trainer.builder().name("Patryk").surname("Tester").build()),
+                Arguments.of(Trainer.builder().name("Joanna").surname("Testerka").build()));
+    }
+
+    public static Stream<Arguments> invalidTrainerName() {
+        return Stream.of(
+                Arguments.of(Trainer.builder().name("").surname("Tester").build()),
+                Arguments.of(Trainer.builder().surname("Tester").name("     ").build()),
+                Arguments.of(Trainer.builder().surname("Tester").name(null).build()));
+    }
+
+    public static Stream<Arguments> invalidTrainerSurname() {
+        return Stream.of(
+                Arguments.of(Trainer.builder().name("Patryk").surname("").build()),
+                Arguments.of(Trainer.builder().name("Patryk").surname("             ").build()),
+                Arguments.of(Trainer.builder().name("Patryk").surname(null).build()));
+    }
+
+    public static Stream<Arguments> invalidTrainerId() {
+        return Stream.of(
+                Arguments.of(-1L, "Trainer with id -1 do not exist"),
+                Arguments.of(0L, "Trainer with id 0 do not exist"),
+                Arguments.of(1000L, "Trainer with id 1000 do not exist"));
+    }
+}
