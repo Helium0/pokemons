5332898f699b9012a4ba3ca06026bf3d45eb2120
Patryk Prentki <patryk.prentki90@gmail.com>
2025-11-30 23:56:56 +0100

    CRUD methods in TrainerService

--- src/main/java/com/example/pokemons/services/TrainerService.java
+++ src/main/java/com/example/pokemons/services/TrainerService.java
@@ -1,12 +1,44 @@
 package com.example.pokemons.services;
 
-import com.example.pokemons.repositories.PokemonRepository;
+import com.example.pokemons.entities.Trainer;
+import com.example.pokemons.helper.ValidatorHelper;
+import com.example.pokemons.repositories.TrainerRepository;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+@AllArgsConstructor
 @Service
 public class TrainerService {
 
-    private PokemonRepository pokemonRepository;
-    private TrainerService trainerService;
+    private final TrainerRepository trainerRepository;
+
+
+    public Trainer createTrainer(Trainer trainer) {
+        trainer.setTrainerName(ValidatorHelper.validateAndTrim(trainer.getName(),"Name"));
+        trainer.setTrainerSurname(ValidatorHelper.validateAndTrim(trainer.getSurname(),"Surname"));
+        return trainerRepository.save(trainer);
+    }
+
+    public void deleteTrainer(Long id) {
+        Trainer trainer = trainerRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException(String.format("Trainer with id %s do not exist", id)));
+//        if (!trainer.getPokemon().isEmpty()) {
+//            throw new IllegalArgumentException("Can not delete trainer with assigned pokemons");  -- TO DO !!
+//        }
+        trainerRepository.delete(trainer);
+    }
+
+    public Trainer updateTrainer(Long id, Trainer updatedTrainer) {
+        Trainer trainer = trainerRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException(String.format("Trainer with id %s do not exist", id)));
+        if (updatedTrainer.getName() != null) {
+            trainer.setTrainerName(ValidatorHelper.validateAndTrim(updatedTrainer.getName(), "Name"));
+        }
+        if (updatedTrainer.getSurname() != null) {
+            trainer.setTrainerSurname(ValidatorHelper.validateAndTrim(updatedTrainer.getSurname(), "Surname"));
+        }
+        return trainerRepository.save(trainer);
+    }
 
 }
