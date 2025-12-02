8c45470cf97103aefcb63569964da82dddb74ade
Patryk Prentki <patryk.prentki90@gmail.com>
2025-12-02 23:52:08 +0100

    Refactored validator methods

--- src/main/java/com/example/pokemons/helper/ValidatorHelper.java
+++ src/main/java/com/example/pokemons/helper/ValidatorHelper.java
@@ -8,24 +8,29 @@ public class ValidatorHelper {
     private static final String BLANK_OR_NULL_ERROR = "%s should not be null or blank";
     private static final String MAX_LENGTH_ERROR = "%s length should not be greater than 100 signs";
     private static final String CAPITALS_ERROR = "%s only first character should be capital, rest should be lowercase";
-    private static final String DOUBLE_MAX_MIN_AMOUNT_ERROR = "%s should not be greater than 100.00 and less than 0.00";
-    private static final String DOUBLE_NULL_NAN_ERROR = "%s should not be null or NaN";
+    private static final String BIG_DECIMAL_MAX_MIN_AMOUNT_ERROR = "%s should not be greater than 100.00 and less than 0.00";
+    private static final String BIG_DECIMAL_NULL_ERROR = "%s should not be null";
     private static final String INTEGER_NULL_ERROR = "%s should not be null";
     private static final String INTEGER_BELOW_ZERO = "%s should be greater or equal 0";
+    private static final String REGEX_VALIDATION = "^[A-ZĄĆĘŁŃÓŚŹŻ][a-ząćęłńóśźż]*$";
 
-    public static String validateAndTrim(String someWord, String fieldName) {
+    public static String validateAndTrimText(String someWord, String fieldName) {
         if (someWord == null || someWord.isBlank()) {
             throw new IllegalArgumentException(String.format(BLANK_OR_NULL_ERROR, fieldName));
         }
-        String trimmedWord = someWord.trim();
-        if (trimmedWord.length() > 100) {
+        String trimmedText = trimText(someWord);
+        if (trimmedText.length() > 100) {
             throw new IllegalArgumentException(String.format(MAX_LENGTH_ERROR, fieldName));
         }
-        if (!trimmedWord.matches("^[A-ZĄĆĘŁŃÓŚŹŻ][a-ząćęłńóśźż]*$")) {
+        if (!someWord.matches(REGEX_VALIDATION)) {
             throw new IllegalArgumentException(String.format(CAPITALS_ERROR, fieldName));
         }
 
-        return trimmedWord.trim();
+        return trimmedText;
+    }
+
+    private static String trimText(String text) {
+        return text.trim();
     }
 
     public static String validateLength(String someText, String fieldName) {
@@ -42,15 +47,15 @@ public class ValidatorHelper {
         return someText.trim();
     }
 
-    public static BigDecimal validateCorrectValue(Double value, String valueField) {
-        if (value == null || value.isNaN()) {
-            throw new IllegalArgumentException(String.format(DOUBLE_NULL_NAN_ERROR, valueField));
+    public static BigDecimal validateCorrectValue(BigDecimal value, String valueField) {
+        if (value == null) {
+            throw new IllegalArgumentException(String.format(BIG_DECIMAL_NULL_ERROR, valueField));
         }
-        if (value > 100.00 || value < 0.00) {
-            throw new IllegalArgumentException(String.format(DOUBLE_MAX_MIN_AMOUNT_ERROR, valueField));
+        if (value.compareTo(BigDecimal.ZERO) < 0 || value.compareTo(new BigDecimal(100)) > 0) {
+            throw new IllegalArgumentException(String.format(BIG_DECIMAL_MAX_MIN_AMOUNT_ERROR, valueField));
         }
 
-        return BigDecimal.valueOf(value).setScale(2, RoundingMode.HALF_UP);
+        return value.setScale(2, RoundingMode.HALF_UP);
     }
 
     public static Integer validateCorrectInteger(Integer integer, String integerField) {
