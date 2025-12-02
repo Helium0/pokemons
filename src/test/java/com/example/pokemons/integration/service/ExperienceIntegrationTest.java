fd523eabd2b92ddf77c0cbfce166678c614d9cdc
Patryk Prentki <patryk.prentki90@gmail.com>
2025-12-02 01:10:12 +0100

    Created integration tests for Experience entity

---
+++ src/test/java/com/example/pokemons/integration/service/ExperienceIntegrationTest.java
@@ -0,0 +1,86 @@
+package com.example.pokemons.integration.service;
+
+import com.example.pokemons.custom.exceptions.ExperienceAlreadyExistException;
+import com.example.pokemons.entities.Experience;
+import com.example.pokemons.repositories.ExperienceRepository;
+import com.example.pokemons.services.ExperienceService;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+public class ExperienceIntegrationTest {
+
+    @Autowired
+    private ExperienceRepository experienceRepository;
+
+    @Autowired
+    private ExperienceService experienceService;
+
+    @ParameterizedTest
+    @MethodSource(value = "com.example.pokemons.testdata.experience.ExperienceIntegrationTestData#validIntegerExperience")
+    public void providedValidExperienceTimeValues(Integer expTime) {
+        //When
+        Experience experience = experienceService.createExperience(expTime);
+
+        //Then
+        assertNotNull(experience);
+        assertNotNull(experience.getId());
+        assertEquals(expTime, experience.getExpTime());
+
+        Optional<Experience> check = experienceRepository.findById(experience.getId());
+
+        assertTrue(check.isPresent());
+        assertEquals(check.get().getExpTime(), expTime);
+    }
+
+    @ParameterizedTest
+    @MethodSource(value = "com.example.pokemons.testdata.experience.ExperienceIntegrationTestData#invalidIntegerExperience")
+    public void invalidExperienceTimeShouldThrowException(Integer expTime) {
+        //When
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+                () -> experienceService.createExperience(expTime));
+
+        //Then
+        assertEquals("Experience time should be greater or equal 0", exception.getMessage());
+    }
+
+    @Test
+    public void experienceTimeAsANullShouldThrowException() {
+        //Given
+        Integer expTime = null;
+
+        //When
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+                () -> experienceService.createExperience(expTime));
+
+        //Then
+        assertEquals("Experience time should not be null", exception.getMessage());
+    }
+
+    @Test
+    public void providedExperienceTimeWhichExistsInDatabaseShouldThrowException() {
+        //Given
+        Integer expTime = 999;
+
+        //When
+        Experience experience = experienceService.createExperience(expTime);
+        assertNotNull(experience.getId());
+        assertNotNull(experience.getExpTime());
+        assertEquals(expTime, experience.getExpTime());
+
+        ExperienceAlreadyExistException exception = assertThrows(ExperienceAlreadyExistException.class,
+                () -> experienceService.createExperience(expTime));
+
+        //Then
+        assertEquals("Experience time: " + expTime + " already exists in DB on ID: " + experience.getId(), exception.getMessage());
+    }
+}
