7b6000c36bf6d9701c75854e4ea7a4191c955edd
Patryk Prentki <patryk.prentki90@gmail.com>
2025-12-03 22:03:35 +0100

    Created TypeRepository for Type Service class

--- src/main/java/com/example/pokemons/repositories/TypeRepository.java
+++ src/main/java/com/example/pokemons/repositories/TypeRepository.java
@@ -3,7 +3,10 @@ package com.example.pokemons.repositories;
 import com.example.pokemons.entities.Type;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 @Repository
 public interface TypeRepository extends JpaRepository<Type, Integer> {
+
+    Optional<Type> findByTypeName(String name);
 }
