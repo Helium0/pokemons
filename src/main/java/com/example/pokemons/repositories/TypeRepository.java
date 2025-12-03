25f8bd8118ea089440c871960c834c645f1aec67
Patryk Prentki <patryk.prentki90@gmail.com>
2025-12-03 23:24:57 +0100

    Fixed repository method name

--- src/main/java/com/example/pokemons/repositories/TypeRepository.java
+++ src/main/java/com/example/pokemons/repositories/TypeRepository.java
@@ -8,5 +8,5 @@ import java.util.Optional;
 @Repository
 public interface TypeRepository extends JpaRepository<Type, Integer> {
 
-    Optional<Type> findByTypeName(String name);
+    Optional<Type> findByName(String name);
 }
