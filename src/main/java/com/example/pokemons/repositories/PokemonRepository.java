9dc13984f46c051608e06762bd411ffcd7d18a95
Patryk Prentki <patryk.prentki90@gmail.com>
2025-12-05 22:56:33 +0100

    PokemonRepository with methods for exist pokemon and List pokemons

--- src/main/java/com/example/pokemons/repositories/PokemonRepository.java
+++ src/main/java/com/example/pokemons/repositories/PokemonRepository.java
@@ -1,10 +1,16 @@
 package com.example.pokemons.repositories;
 
 import com.example.pokemons.entities.Pokemon;
+import com.example.pokemons.entities.Type;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.math.BigDecimal;
+import java.util.List;
 
 @Repository
 public interface PokemonRepository extends JpaRepository<Pokemon, Long> {
+
+    boolean existsByNameAndPowerAndDescriptionAndType(String name, BigDecimal power, String desc, Type type);
+    List<Pokemon> findByNameAndPowerAndType(String name, BigDecimal power, Type type);
 
 }
