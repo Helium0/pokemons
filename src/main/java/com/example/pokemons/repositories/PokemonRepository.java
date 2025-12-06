e052e2affc7568d345a2e932eed102091bf1ea78
Patryk Prentki <patryk.prentki90@gmail.com>
2025-12-06 23:53:35 +0100

    Added JPA`s methods for find pokemons ASC and DESC

--- src/main/java/com/example/pokemons/repositories/PokemonRepository.java
+++ src/main/java/com/example/pokemons/repositories/PokemonRepository.java
@@ -2,7 +2,11 @@ package com.example.pokemons.repositories;
 
 import com.example.pokemons.entities.Pokemon;
 import com.example.pokemons.entities.Type;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import java.math.BigDecimal;
 import java.util.List;
@@ -11,6 +15,14 @@ import java.util.List;
 public interface PokemonRepository extends JpaRepository<Pokemon, Long> {
 
     boolean existsByNameAndPowerAndDescriptionAndType(String name, BigDecimal power, String desc, Type type);
-    List<Pokemon> findByNameAndPowerAndType(String name, BigDecimal power, Type type);
 
+    @Query(value = "SELECT p FROM Pokemon p WHERE p.type = :type")
+    List<Pokemon> findByType(@Param("type") Type type);
+
+    @Modifying(flushAutomatically = true, clearAutomatically = true)
+    @Transactional
+    @Query(value = "DELETE FROM Pokemon WHERE name = :name AND power = :power AND type = :type")
+    void deletePokemonByNameAndPowerAndType(@Param("name") String name, @Param("power") BigDecimal power, @Param("type") Type type);
+
+    List<Pokemon> findByTypeOrderByPowerDesc(Type type);
 }
