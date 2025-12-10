b39d9c9a8c32a972fb0f1e63a6d3872927e7067f
Patryk Prentki <patryk.prentki90@gmail.com>
2025-12-10 20:19:02 +0100

    Test data for PokemonServiceUnitTest

--- src/test/java/com/example/pokemons/testdata/pokemon/PokemonServiceUnitTestData.java
+++ src/test/java/com/example/pokemons/testdata/pokemon/PokemonServiceUnitTestData.java
@@ -11,9 +11,11 @@ public class PokemonServiceUnitTestData {
     private static final Type ELECTRIC_TYPE = Type.builder().name("Electric").build();
     private static final Type EARTH_TYPE = Type.builder().name("Earth").build();
     private static final Type WATER_TYPE = Type.builder().name("Water").build();
-    private static final Type FIRE_TYPE = Type.builder().name("Fire").build();
     private static final Type NULL_TYPE_NAME = Type.builder().name(null).build();
     private static final Type BLANK_TYPE_NAME = Type.builder().name("").build();
+    private static final String NULL_OR_BLANK_ERROR = "Name should not be null or blank";
+    private static final String MAX_OR_LESS_ERROR = "Power should not be greater than 100.00 and less than 0.00";
+    private static final String CAPITAL_ERROR = "Name only first character should be capital, rest should be lowercase";
 
 
     public static Stream<Arguments> validPokemonData() {
@@ -25,16 +27,16 @@ public class PokemonServiceUnitTestData {
     }
     public static Stream<Arguments> invalidPokemonData() {
         return Stream.of(
-                Arguments.of(null, new BigDecimal("50.00"), null, WATER_TYPE, "Name should not be null or blank"),
-                Arguments.of("", new BigDecimal("90.00"), null, WATER_TYPE, "Name should not be null or blank"),
-                Arguments.of("           ", new BigDecimal("90.00"), null, WATER_TYPE, "Name should not be null or blank"),
-                Arguments.of("a".repeat(100), new BigDecimal("56.00"), null, WATER_TYPE, "Name only first character should be capital, rest should be lowercase"),
+                Arguments.of(null, new BigDecimal("50.00"), null, WATER_TYPE, NULL_OR_BLANK_ERROR),
+                Arguments.of("", new BigDecimal("90.00"), null, WATER_TYPE, NULL_OR_BLANK_ERROR),
+                Arguments.of("           ", new BigDecimal("90.00"), null, WATER_TYPE, NULL_OR_BLANK_ERROR),
+                Arguments.of("a".repeat(100), new BigDecimal("56.00"), null, WATER_TYPE, CAPITAL_ERROR),
                 Arguments.of("A" + "b".repeat(100), new BigDecimal("56.00"), null, WATER_TYPE, "Name length should not be greater than 100 signs"),
-                Arguments.of("A".repeat(100), new BigDecimal("56.00"), null, WATER_TYPE, "Name only first character should be capital, rest should be lowercase"),
-                Arguments.of("Pikachu", new BigDecimal("100.01"), "Kind pokemon", ELECTRIC_TYPE, "Power should not be greater than 100.00 and less than 0.00"),
-                Arguments.of("Pikachu", new BigDecimal("100.005"), "Kind pokemon", ELECTRIC_TYPE, "Power should not be greater than 100.00 and less than 0.00"),
-                Arguments.of("Pikachu", new BigDecimal("-0.01"), "Kind pokemon", ELECTRIC_TYPE, "Power should not be greater than 100.00 and less than 0.00"),
-                Arguments.of("Pikachu", new BigDecimal("-0.001"), "Kind pokemon", ELECTRIC_TYPE, "Power should not be greater than 100.00 and less than 0.00"),
+                Arguments.of("A".repeat(100), new BigDecimal("56.00"), null, WATER_TYPE, CAPITAL_ERROR),
+                Arguments.of("Pikachu", new BigDecimal("100.01"), "Kind pokemon", ELECTRIC_TYPE, MAX_OR_LESS_ERROR),
+                Arguments.of("Pikachu", new BigDecimal("100.005"), "Kind pokemon", ELECTRIC_TYPE, MAX_OR_LESS_ERROR),
+                Arguments.of("Pikachu", new BigDecimal("-0.01"), "Kind pokemon", ELECTRIC_TYPE, MAX_OR_LESS_ERROR),
+                Arguments.of("Pikachu", new BigDecimal("-0.001"), "Kind pokemon", ELECTRIC_TYPE, MAX_OR_LESS_ERROR),
                 Arguments.of("Pikachu", new BigDecimal("35.00"), "Kind pokemon".repeat(40), ELECTRIC_TYPE, "Description length should not be greater than 100 signs"),
                 Arguments.of("Pikachu", new BigDecimal("90.00"), "Kind pokemon", NULL_TYPE_NAME, "Type should not be null or blank"),
                 Arguments.of("Pikachu", new BigDecimal("90.00"), "Kind pokemon", BLANK_TYPE_NAME, "Type should not be null or blank"));
