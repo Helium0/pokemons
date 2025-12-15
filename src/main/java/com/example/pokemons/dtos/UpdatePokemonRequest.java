14845c7030748a652fcdd17fdff25fd19f6d3feb
Patryk Prentki <patryk.prentki90@gmail.com>
2025-12-15 19:17:25 +0100

    Renamed request pokemon DTO

--- src/main/java/com/example/pokemons/dtos/UpdatePokemonRequest.java
+++ src/main/java/com/example/pokemons/dtos/UpdatePokemonRequest.java
@@ -5,7 +5,7 @@ import lombok.Data;
 import java.math.BigDecimal;
 
 @Data
-public class PokemonRequest {
+public class UpdatePokemonRequest {
 
     private final String description;
     private final BigDecimal power;
